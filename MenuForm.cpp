4bbb39b316185e06840c6b895fa3c1bb41dc41cf
LoRy24 <lorytherock24tutorial@gmail.com>
2021-06-30 16:21:18 +0200

    Created new windows form

---
+++ MenuForm.cpp
@@ -0,0 +1,12 @@
+#include "MenuForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void main(array<String^>^ args) {
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	HolyCode::MenuForm form;
+	Application::Run(% form);
+}
